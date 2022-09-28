734bf1d75de994edf168adb974b2d06f60d988a4
James Graca-Jones <jamesgracajones@gmail.com>
2022-09-28 16:19:09 +0100

    Add <p> and <h3> elements to page and style them

--- the-odin-project/javascript-basics-11/javascript.js
+++ the-odin-project/javascript-basics-11/javascript.js
@@ -1,7 +1,22 @@
 const container = document.querySelector('#container');
 
+
 const content = document.createElement('div');
 content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
 
 container.appendChild(content);
+
+
+const para = document.createElement('p');
+para.style.color = 'red'
+para.textContent = "Hey, I'm red!";
+
+container.appendChild(para);
+
+
+const lvl3Heading = document.createElement('h3');
+lvl3Heading.style.color = 'blue'
+lvl3Heading.textContent = "I'm a blue h3!"
+
+container.appendChild(lvl3Heading)
