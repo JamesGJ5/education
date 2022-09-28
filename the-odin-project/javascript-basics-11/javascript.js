b4f78036ee2ccf7dd040a6cd8fee591ed24d5b03
James Graca-Jones <jamesgracajones@gmail.com>
2022-09-28 17:26:31 +0100

    Complete Exercise 3

--- the-odin-project/javascript-basics-11/javascript.js
+++ the-odin-project/javascript-basics-11/javascript.js
@@ -20,3 +20,20 @@ lvl3Heading.style.color = 'blue'
 lvl3Heading.textContent = "I'm a blue h3!"
 
 container.appendChild(lvl3Heading)
+
+
+const div2 = document.createElement('div');
+div2.style.border = 'solid black';
+div2.style.backgroundColor = 'pink';
+
+const lvl1Heading = document.createElement('h1');
+lvl1Heading.textContent = "I'm in a div!"
+
+div2.appendChild(lvl1Heading)
+
+const para2 = document.createElement('p')
+para2.textContent = 'ME TOO!'
+
+div2.appendChild(para2)
+
+container.appendChild(div2);
