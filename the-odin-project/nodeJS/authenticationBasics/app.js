163270821088cb86442f850d000d11c8820b45c0
James Graca-Jones <jamesgracajones@gmail.com>
2023-03-15 15:08:24 +0000

    Complete lesson

---
+++ the-odin-project/nodeJS/authenticationBasics/app.js
@@ -0,0 +1,139 @@
+/////// app.js
+
+const express = require("express");
+const path = require("path");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const bcrypt = require("bcryptjs");
+
+const mongoDb = "mongodb+srv://jamesgracajones:9b3h9YAwSf3X44OO@databaseforlesson.wc2lour.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "mongo connection error"));
+
+const User = mongoose.model(
+  "User",
+  new Schema({
+    username: { type: String, required: true },
+    password: { type: String, required: true }
+  })
+);
+
+const app = express();
+app.set("views", __dirname);
+app.set("view engine", "ejs");
+
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+
+passport.use(
+  new LocalStrategy(async(username, password, done) => {
+    try {
+      const user = await User.findOne({ username: username });
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      };
+    // The commented parts below refer to when hashing the password was not done.
+    // Now one has to unhash the stored password to compare it to the tried one.
+    //   if (user.password !== password) {
+    //     return done(null, false, { message: "Incorrect password" });
+    //   };
+    //   return done(null, user);
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (err) {
+            throw err;
+        }
+        else if (res) {
+          // passwords match! log user in
+          return done(null, user)
+        } else {
+          // passwords do not match!
+          return done(null, false, { message: "Incorrect password" })
+        }
+      });
+    } catch(err) {
+      return done(err);
+    };
+  })
+);
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async function(id, done) {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch(err) {
+    done(err);
+  };
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+});
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+// No need for the commented line below because details of current user are being stored 
+// in 'locals' (see relevant middleware above somewhere)
+//   res.render("index", { user: req.user });
+  res.render("index");
+});
+
+app.post(
+  "/log-in",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/"
+  })
+);
+
+app.get("/log-out", (req, res, next) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
+
+app.get("/sign-up", (req, res) => res.render("sign-up-form"));
+
+app.post("/sign-up", async (req, res, next) => {
+//   try {
+//     const user = new User({
+//       username: req.body.username,
+//       password: req.body.password
+//     });
+//     const result = await user.save();
+//     res.redirect("/");
+//   } catch(err) {
+//     return next(err);
+//   };
+  try {
+      bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+          if (err) {
+              throw err;
+          }
+          const user = new User({
+              username: req.body.username,
+              password: hashedPassword
+          });
+          const result = await user.save();
+          res.redirect("/");
+      });
+  } catch(err) {
+      return next(err);
+  };
+});
+
+app.listen(3000, () => console.log("app listening on port 3000!"));
